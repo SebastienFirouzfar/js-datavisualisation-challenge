008b395e709f390cfb8d45b8d15031b6f179bc79
Firouzfar <firouzfar.sebastien@gmail.com>
2020-11-10 16:56:50 +0100

    Suite exo 2

--- index.js
+++ index.js
@@ -69,3 +69,58 @@ new Chart(document.getElementById("line-chart"), {
     }
 });
 
+/**********************************************Exo 2************************************/
+let country2 = document.querySelectorAll("#table2 thead tr")[0]
+let tabCountry = []; 
+Array.from(country2.children).forEach(function(countryName){
+    if(countryName.innerHTML.length > 0){
+        tabCountry.push(countryName.innerText); 
+    }
+})
+console.log(tabCountry);
+
+let tab2 = []
+dataset2 = Array.from(document.querySelectorAll("#table1 thead tr"))
+
+
+dataset2.forEach(function (datas2) {
+    let object = {}
+    let arData2 = []
+    //declarons un tableau d'enfant
+    data = Array.from(datas2.children);
+    //on supprime le 1 et le 2 du tableau des données
+    data.shift()
+    data.shift()
+    //nous parcourons les données (les valeurs pour chaque pays)
+    data.forEach(function (x) {
+        arData2.push(parseInt(x.innerText))
+    })
+    //Enregistrons les données dans l'object
+    object.data = arData2
+    object.label = datas2.children[1].innerText
+    object.borderColor = getRandomColor()
+    //Une fois que nous avons enregistre les nouvelles données nous 
+    //pouvons le mettre dans le tableau
+    tab2.push(object); 
+
+    console.log(arData2);
+})
+
+console.log(tab2);
+
+
+new Chart(document.getElementById("bar-chart-grouped"), {
+    type: 'bar',
+    data: {
+      labels: tabCountry,
+      datasets: tab2
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Population growth (millions)'
+      }
+    }
+});
+
+
